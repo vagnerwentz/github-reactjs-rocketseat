5faa2666ddbc45bf863b6188b2183c8a478392da
vagnerwentz <wentz.vagner@gmail.com>
2020-03-01 03:01:01 -0300

    Fixed: Some rules fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,8 @@ module.exports = {
           'warn',
           { extensions: ['.jsx', '.js'] }
       ],
-      'import/prefer-default-export': 'off'
+      'import/prefer-default-export': 'off',
+      'react/prefer-stateless-function': 'off',
+      'react/state-in-constructor': 'off'
   },
 };
